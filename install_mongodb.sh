fa38dd88cddc681338679ac009d9d05949830f42
PraiseBeToScience <pbts.reddit@gmail.com>
2017-01-13 07:59:55 +0000

    Mongodb install script

---
+++ install_mongodb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+apt-get update
+apt-get install -y mongodb-org
+service mongod start
+tail /var/log/mongodb/mongod.log
