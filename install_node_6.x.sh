55cf7ad75b46b38beb4959fcef40f6b4a77a019c
PraiseBeToScience <pbts.reddit@gmail.com>
2017-01-14 04:25:04 +0000

    install gulp globally with node

--- install_node_6.x.sh
+++ install_node_6.x.sh
@@ -4,4 +4,5 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 npm i npm --upgrade -g
+npm i --global gulp
  
